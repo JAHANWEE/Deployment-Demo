62b3da900f7796ed03a5bb05a871c30a8b474465
Jahanwee . <jahan@MacBook-Air.local>
2026-06-07 18:42:33 +0530

    chore:whoami route added

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/' , (_,res)=>{
     res.send("Hello from our tiny deployment app from v4.2")
 })
 
+app.get('/whoami' , (_,res)=>{
+    res.send(`Hey peeps,
+        Welcome to my mini deployment website.
+        i am a developer who is just trying to get a hang of things.
+        So be nice to me , my dog also says "hi" `)
+})
+
 app.get('/version' , (req,res)=>{
     res.json( {version : `You are currently running on ${VERSION}`});
 } )
